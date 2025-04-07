9a02fb427891acb1f3c321cd1d3fe1874a2f8054
Jiyeong Son <sgsgk1213@naver.com>
2025-04-07 21:26:53 +0900

    chore: 프로젝트 초기 설정 및 리소스 파일 추가

---
+++ src/test/java/com/skala/spring_stockmarket/SpringStockmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skala.spring_stockmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStockmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
